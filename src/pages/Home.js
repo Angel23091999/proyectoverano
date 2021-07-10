ae4e92b252fcbc873f9ccc7c8369734a7e16ae51
Angel Geronimo <aarcosgeronimo@gmail.com>
2021-07-10 00:17:53 -0500

    Agregando nuevas funciones

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Menu from "../Componentes/opcionMenu/Menu";
+import CarrouselInicio from '../Componentes/Carrousel/CarrouselInicio';
+//import FormDatos from '../Componentes/FormDatos';
+
+function Home() {
+  return (
+    <div>
+      <Menu />
+      <CarrouselInicio/>
+    </div>
+  )
+}
+export default Home;
